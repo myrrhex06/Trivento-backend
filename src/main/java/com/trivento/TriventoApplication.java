cd9e8dc693f0e03930e432a2a4ffc7d938b1b35a
myrrhex06 <gitlwer210@gmail.com>
2025-08-25 18:01:44 +0900

    Initial commit

---
+++ src/main/java/com/trivento/TriventoApplication.java
@@ -0,0 +1,13 @@
+package com.trivento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trivento/TriventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
